090d81649b302d3477e477716a86d56670fb3ec2
leitto-mk <leitto.ibtj@gmail.com>
2024-09-22 04:32:20 +0800

    Update Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <script>
-import moment from 'moment';
 import { useLayout } from '@/layout/composables/layout';
+import moment from 'moment';
 
 export default {
     setup() {
@@ -105,11 +105,13 @@ export default {
 </script>
 
 <template>
-    <FloatingConfigurator />
-    <Fluid class="layout-menu bg-slate-100 dark:bg-zinc-900 h-screen">
+    <Fluid class="layout-menu bg-slate-100 dark:bg-zinc-900 min-h-screen md:min-h-0 md:h-auto lg:h-screen">
+        <div class="flex flex-row-reverse">
+            <Button label="logout" severity="secondary"></Button>
+        </div>
         <div id="home" class="landing-wrapper overflow-hidden">
             <div class="py-2 px-2 mx-0 md:mx-12 lg:mx-20 lg:px-20 flex items-center justify-between relative lg:static">
-                <a class="flex items-center" href="/dashboard">
+                <a class="flex items-center" href="/">
                     <div class="flex items-center justify-center mr-3">
                         <img v-if="isDarkTheme" src="@/assets/dashboard-light.png" alt="" class="w-[75%] lg:w-[45%]" />
                         <img v-else src="@/assets/dashboard-dark.png" alt="" class="w-[75%] lg:w-[45%]" />
@@ -121,8 +123,9 @@ export default {
                 </div> -->
             </div>
 
-            <div class="flex flex-row justify-between mx-20">
+            <div class="flex flex-col justify-between mx-20 gap-2">
                 <h1 class="lg:text-4xl font-bold text-gray-900 dark:text-slate-300 leading-tight"><span class="font-light block">Selamat Datang</span>MERCHANT_NAME</h1>
+                <span class="text-base rounded text-slate-100 text-center uppercase p-1 bg-green-500 w-20">ADMIN</span>
             </div>
 
             <Menubar class="mx-20 lg:mx-20 mt-5 lg:mt-10">
@@ -138,33 +141,100 @@ export default {
                 </template>
             </Menubar>
 
-            <div class="flex flex-row justify-center mx-8 lg:mx-20 gap-3 lg:gap-14 mt-5 lg:mt-10 mb-10 lg:mb-20">
-                <Button label="QRIS" icon="pi pi-qrcode" severity="danger" rounded class="w-48" @click="emptyState()" />
+            <div class="flex flex-row justify-center mx-8 lg:mx-20 gap-3 lg:gap-14 mt-5 lg:mt-10">
                 <Button
-                    label="Transaksi"
-                    icon="pi pi-credit-card"
-                    severity="warn"
-                    rounded
-                    class="w-48"
-                    @click="
-                        emptyState();
-                        transactions_state = !transactions_state;
-                    "
-                />
-                <Button
-                    label="Terminal"
+                    label="User"
                     icon="pi pi-desktop"
                     severity="info"
+                    :outlined="terminal_state ? false : true"
                     rounded
-                    class="w-48"
+                    :class="terminal_state ? 'w-40 h-14 animate-scalein' : 'w-30 h-14'"
                     @click="
                         emptyState();
                         terminal_state = !terminal_state;
                     "
                 />
+                <Button
+                    label="QRIS"
+                    icon="pi pi-qrcode"
+                    severity="danger"
+                    :outlined="qris_state ? false : true"
+                    rounded
+                    :class="qris_state ? 'w-40 h-14 animate-scalein' : 'w-30 h-14'"
+                    @click="
+                        emptyState();
+                        qris_state = !qris_state;
+                    "
+                />
+                <Button
+                    label="Transaksi"
+                    icon="pi pi-credit-card"
+                    severity="success"
+                    :outlined="transactions_state ? false : true"
+                    rounded
+                    :class="transactions_state ? 'w-40 h-14 animate-scalein' : 'w-30 h-14'"
+                    @click="
+                        emptyState();
+                        transactions_state = !transactions_state;
+                    "
+                />
             </div>
 
-            <div v-if="transactions_state" class="flex flex-col justify-center mx-12 lg:mx-80 lg:mt-10 lg:mb-28">
+            <div v-if="terminal_state" class="flex flex-row justify-center mx-8 lg:mx-20 gap-3 lg:gap-14 mt-10 mb-10">
+                <div class="flex flex-wrap w-40">
+                    <Button label="Tambah" icon="pi pi-user-plus" severity="contrast" outlined rounded class="hover:animate-scalein" />
+                </div>
+            </div>
+
+            <div v-if="terminal_state" class="grid lg:grid-cols-3 justify-center gap-2 lg:gap-16 lg:mx-80 mt-5">
+                <div class="border border-slate-200 rounded-lg p-5 group hover:transition hover:ease-in-out hover:delay-150 hover:border hover:rounded-xl hover:p-10 hover:border-orange-300 mb-3 lg:mb-20">
+                    <div class="flex flex-row justify-between gap-3">
+                        <div class="flex flex-col gap-2">
+                            <Tag value="A01" severity="info" class="w-16 h-15"></Tag>
+                            <Avatar label="S" class="mr-2" size="xlarge" v-tooltip.bottom="'Static'" />
+                        </div>
+                        <div class="flex flex-col">
+                            <div class="flex flex-row-reverse mb-6">
+                                <Tag value="USER" severity="warn" class="w-16 h-15 group-hover:text-lg"></Tag>
+                            </div>
+                            <span class="text-xs font-normal text-slate-700 dark:text-slate-300 uppercase text-right">Device:</span>
+                            <span class="group-hover:text-sm text-xl group-hover:font-extrabold hover:text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Galaxy S24 Ultra</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="border border-slate-200 rounded-lg p-5 group hover:transition hover:ease-in-out hover:delay-150 hover:border hover:rounded-xl hover:p-10 hover:border-orange-300 mb-3 lg:mb-20">
+                    <div class="flex flex-row justify-between gap-3">
+                        <div class="flex flex-col gap-2">
+                            <Tag value="A02" severity="info" class="w-16 h-15"></Tag>
+                            <Avatar label="D" class="mr-2" size="xlarge" v-tooltip.bottom="'Dinamis'" />
+                        </div>
+                        <div class="flex flex-col">
+                            <div class="flex flex-row-reverse mb-6">
+                                <Tag value="USER" severity="warn" class="w-16 h-15 group-hover:text-lg"></Tag>
+                            </div>
+                            <span class="text-xs font-normal text-slate-700 dark:text-slate-300 uppercase text-right">Device:</span>
+                            <span class="group-hover:text-sm text-xl group-hover:font-extrabold hover:text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Galaxy S24 Ultra</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="border border-slate-200 rounded-lg p-5 group hover:transition hover:ease-in-out hover:delay-150 hover:border hover:rounded-xl hover:p-10 hover:border-orange-300 mb-3 lg:mb-20">
+                    <div class="flex flex-row justify-between gap-3">
+                        <div class="flex flex-col gap-2">
+                            <Tag value="A03" severity="info" class="w-16 h-15"></Tag>
+                            <Avatar label="D" class="mr-2" size="xlarge" v-tooltip.bottom="'Dinamis'" />
+                        </div>
+                        <div class="flex flex-col">
+                            <div class="flex flex-row-reverse mb-6">
+                                <Tag value="USER" severity="warn" class="w-16 h-15 group-hover:text-lg"></Tag>
+                            </div>
+                            <span class="text-xs font-normal text-slate-700 dark:text-slate-300 uppercase text-right">Device:</span>
+                            <span class="group-hover:text-sm text-xl group-hover:font-extrabold hover:text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Galaxy S24 Ultra</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div v-if="transactions_state" class="flex flex-col justify-center mx-12 lg:mx-80 mt-10 lg:mt-20 lg:mb-28">
                 <div v-for="trx in transactions" :key="trx.id" class="flex flex-col hover:transition hover:ease-in-out hover:delay-150 hover:border hover:rounded-2xl hover:p-3 lg:hover:p-10 hover:border-orange-300 mb-6 lg:mb-20">
                     <div class="flex flex-row justify-between gap-1">
                         <div class="flex flex-row">
@@ -189,52 +259,8 @@ export default {
                 </div>
             </div>
 
-            <div v-if="terminal_state" class="grid lg:grid-cols-3 justify-center gap-2 lg:gap-16 lg:mx-80 lg:mt-10">
-                <div class="border border-slate-200 rounded-lg p-5 group hover:transition hover:ease-in-out hover:delay-150 hover:border hover:rounded-xl hover:p-10 hover:border-orange-300 mb-3 lg:mb-20">
-                    <div class="flex flex-row justify-between gap-3">
-                        <div class="flex flex-col gap-2">
-                            <Tag value="A01" severity="info" class="w-16 h-15"></Tag>
-                            <Avatar icon="pi pi-th-large" class="mr-2" size="xlarge" />
-                        </div>
-                        <div class="flex flex-col">
-                            <div class="flex flex-row-reverse mb-6">
-                                <Tag value="USER" severity="warn" class="w-16 h-15 group-hover:text-lg"></Tag>
-                            </div>
-                            <span class="text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Device:</span>
-                            <span class="group-hover:text-sm text-xl group-hover:font-extrabold hover:text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Galaxy S24 Ultra</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="border border-slate-200 rounded-lg p-5 group hover:transition hover:ease-in-out hover:delay-150 hover:border hover:rounded-xl hover:p-10 hover:border-orange-300 mb-3 lg:mb-20">
-                    <div class="flex flex-row justify-between gap-3">
-                        <div class="flex flex-col gap-2">
-                            <Tag value="A01" severity="info" class="w-16 h-15"></Tag>
-                            <Avatar icon="pi pi-th-large" class="mr-2" size="xlarge" />
-                        </div>
-                        <div class="flex flex-col">
-                            <div class="flex flex-row-reverse mb-6">
-                                <Tag value="USER" severity="warn" class="w-16 h-15 group-hover:text-lg"></Tag>
-                            </div>
-                            <span class="text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Device:</span>
-                            <span class="group-hover:text-sm text-xl group-hover:font-extrabold hover:text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Galaxy S24 Ultra</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="border border-slate-200 rounded-lg p-5 group hover:transition hover:ease-in-out hover:delay-150 hover:border hover:rounded-xl hover:p-10 hover:border-orange-300 mb-3 lg:mb-20">
-                    <div class="flex flex-row justify-between gap-3">
-                        <div class="flex flex-col gap-2">
-                            <Tag value="A01" severity="info" class="w-16 h-15"></Tag>
-                            <Avatar icon="pi pi-th-large" class="mr-2" size="xlarge" />
-                        </div>
-                        <div class="flex flex-col">
-                            <div class="flex flex-row-reverse mb-6">
-                                <Tag value="USER" severity="warn" class="w-16 h-15 group-hover:text-lg"></Tag>
-                            </div>
-                            <span class="text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Device:</span>
-                            <span class="group-hover:text-sm text-xl group-hover:font-extrabold hover:text-xs font-medium text-slate-700 dark:text-slate-300 uppercase text-right">Galaxy S24 Ultra</span>
-                        </div>
-                    </div>
-                </div>
+            <div v-if="qris_state" class="flex flex-row justify-center mx-8 lg:mx-20 gap-3 lg:gap-14 mt-5 lg:mt-14 mb-10">
+                <img src="@/assets/qris.jpeg" alt="" class="lg:w-[50%]" />
             </div>
         </div>
     </Fluid>
